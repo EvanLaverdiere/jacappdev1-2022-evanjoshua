9a5808bc0c7499dbfd3eed49aa0c4c085a8408a0
EvanLaverdiere <0860347@johnabbottcollege.net>
2022-03-08 11:58:56 -0500

    Commenting out references to obsolete XML Expenses test file in GetBudgetItemsByMonth tests.

--- BudgetTesting/TestHomeBudget_GetBudgetItemsByMonth.cs
+++ BudgetTesting/TestHomeBudget_GetBudgetItemsByMonth.cs
@@ -9,7 +9,7 @@ namespace BudgetCodeTests
     [Collection("Sequential")]
     public class TestHomeBudget_GetBudgetItemsByMonth
     {
-        string testInputFile = TestConstants.testExpensesInputFile;
+        //string testInputFile = TestConstants.testExpensesInputFile;
         
 
         // ========================================================================
@@ -21,7 +21,7 @@ namespace BudgetCodeTests
         {
             // Arrange
             string folder = TestConstants.GetSolutionDir();
-            string inFile = TestConstants.GetSolutionDir() + "\\" + testInputFile;
+            //string inFile = TestConstants.GetSolutionDir() + "\\" + testInputFile;
             String goodDB = $"{folder}\\{TestConstants.testDBInputFile}";
             String messyDB = $"{folder}\\messy.db";
             System.IO.File.Copy(goodDB, messyDB, true);
@@ -58,7 +58,7 @@ namespace BudgetCodeTests
         {
             // Arrange
             string folder = TestConstants.GetSolutionDir();
-            string inFile = TestConstants.GetSolutionDir() + "\\" + testInputFile;
+            //string inFile = TestConstants.GetSolutionDir() + "\\" + testInputFile;
             String goodDB = $"{folder}\\{TestConstants.testDBInputFile}";
             String messyDB = $"{folder}\\messy.db";
             System.IO.File.Copy(goodDB, messyDB, true);
@@ -94,7 +94,7 @@ namespace BudgetCodeTests
         {
             // Arrange
             string folder = TestConstants.GetSolutionDir();
-            string inFile = TestConstants.GetSolutionDir() + "\\" + testInputFile;
+            //string inFile = TestConstants.GetSolutionDir() + "\\" + testInputFile;
             String goodDB = $"{folder}\\{TestConstants.testDBInputFile}";
             String messyDB = $"{folder}\\messy.db";
             System.IO.File.Copy(goodDB, messyDB, true);
@@ -134,7 +134,7 @@ namespace BudgetCodeTests
         {
             // Arrange
             string folder = TestConstants.GetSolutionDir();
-            string inFile = TestConstants.GetSolutionDir() + "\\" + testInputFile;
+            //string inFile = TestConstants.GetSolutionDir() + "\\" + testInputFile;
             String goodDB = $"{folder}\\{TestConstants.testDBInputFile}";
             String messyDB = $"{folder}\\messy.db";
             System.IO.File.Copy(goodDB, messyDB, true);
