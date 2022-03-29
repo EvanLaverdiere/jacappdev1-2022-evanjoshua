7a449051ba3ec00ddee536452620767616c46190
EvanLaverdiere <0860347@johnabbottcollege.net>
2022-03-29 13:53:35 -0400

    Added documentation to Presenter.CreateBudget.

--- WpfHomeBudget/WpfHomeBudget/Presenter.cs
+++ WpfHomeBudget/WpfHomeBudget/Presenter.cs
@@ -20,6 +20,12 @@ namespace WpfHomeBudget
             //budget = new HomeBudget();
         }
         // methods
+        /// <summary>
+        /// Initializes a budget database.
+        /// </summary>
+        /// <remarks>Creates a new database or opens an existing database based on boolean value passed in from the view.</remarks>
+        /// <param name="dbName">The database filename.</param>
+        /// <param name="newDatabase">True if this is a new database, false if this is an existing database.</param>
         public void CreateBudget(string dbName, bool newDatabase)
         {
             budget = new HomeBudget(dbName, newDatabase);
