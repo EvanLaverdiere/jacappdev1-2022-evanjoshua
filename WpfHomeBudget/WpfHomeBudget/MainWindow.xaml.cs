0141eee457d4928d2e5e874bd7b958c1988693a0
Abdel <abdessalam.aithaqi@gmail.com>
2022-03-31 13:47:00 -0400

    Open Dialog box on click

--- WpfHomeBudget/WpfHomeBudget/MainWindow.xaml.cs
+++ WpfHomeBudget/WpfHomeBudget/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Budget;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
 
 namespace WpfHomeBudget
 {
@@ -21,14 +24,34 @@ namespace WpfHomeBudget
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void createDbBtn_Click(object sender, RoutedEventArgs e)
+        private void CreateDbBtn_Click(object sender, RoutedEventArgs e)
         {
+            // Create a new FolderBrowserDialog object
+            FolderBrowserDialog openFolderDlg = new FolderBrowserDialog()
+            {
+                //SelectedPath = "%userprofile%\\documents",
+                RootFolder = Environment.SpecialFolder.MyDocuments,
+                Description = "Select the folder in which you want to store your new Budget",
+            };
 
+            // Show the FolderBrowserDialog by calling ShowDialog method
+            DialogResult result = openFolderDlg.ShowDialog();
+
+            // Get the selected file name
+            if (result.ToString() != string.Empty)
+            {
+                string directory = openFolderDlg.SelectedPath;
+            }
+
+            // Pass the value to the entry window so that it knows what to give the presenter
+            // open the new entry winow and close this one
         }
 
         private void existingDbBtn_Click(object sender, RoutedEventArgs e)
