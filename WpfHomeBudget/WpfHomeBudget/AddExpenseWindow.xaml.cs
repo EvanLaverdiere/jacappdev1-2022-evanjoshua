8efde993653bbacdd5353ece0bbf3a4cd6b5c227
EvanLaverdiere <0860347@johnabbottcollege.net>
2022-04-04 11:48:43 -0400

    Implemented AddExpenseWindow's btnCancel_Click(). Added documentation for method.

--- WpfHomeBudget/WpfHomeBudget/AddExpenseWindow.xaml.cs
+++ WpfHomeBudget/WpfHomeBudget/AddExpenseWindow.xaml.cs
@@ -38,9 +38,16 @@ namespace WpfHomeBudget
             presenter.CreateNewExpense(date, categoryId, amount, description);
         }
 
+        /// <summary>
+        /// Prompts user to confirm that they want to cancel adding a new expense to the budget.
+        /// If user chooses yes, method closes this window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
-
+            if (MessageBox.Show("Are you sure you want to cancel adding this new expense?", "CONFIRM CANCELATION", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                this.Close();
         }
     }
 }
