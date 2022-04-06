36fd4854a270bc46b071b9b14036b3b129a71a42
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2022-04-05 20:13:17 -0400

    Created a TestView class to test the functionality of the interface methods.

--- WpfHomeBudget/TestProject/TestPresenter.cs
+++ WpfHomeBudget/TestProject/TestPresenter.cs
@@ -3,9 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfHomeBudget;
 
 namespace TestProject
 {
+    public class TestView : IViewable
+    {
+        public void ClearError()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearForm()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearSelection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Refresh()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowBudgetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowError(string error)
+        {
+            throw new NotImplementedException();
+        }
+    }
     class TestPresenter
     {
     }
