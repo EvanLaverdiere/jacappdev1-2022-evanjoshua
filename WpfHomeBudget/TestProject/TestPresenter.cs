c75b351a84712ff54295a6e0efa38af89916a568
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2022-04-23 16:14:17 -0400

    Wrote test to confirm that GetBudgetDictionaryByCategoryAndMonth calls the interface's ShowBudgetItems() method.

--- WpfHomeBudget/TestProject/TestPresenter.cs
+++ WpfHomeBudget/TestProject/TestPresenter.cs
@@ -233,6 +233,28 @@ namespace TestProject
         }
 
         [Fact]
+        public void Test_GetBudgetDictionaryByCategoryAndMonthCallsShowBudgetItems()
+        {
+            // Arrange
+            TestView testView = new TestView();
+            Presenter presenter = new Presenter(testView);
+
+            // Manually create a database so the Presenter can actually retrieve something.
+            string folder = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
+            String goodDB = $"{folder}\\{TEST_DB_INPUT_FILE}";
+            String messyDB = $"{folder}\\messy.db";
+            System.IO.File.Copy(goodDB, messyDB, true);
+            presenter.CreateBudget(messyDB, false);
+
+            // Assert
+            presenter.GetBudgetDictionaryByCategoryAndMonth(null, null, false, 0);
+
+            // Act
+            Assert.True(testView.calledShowBudgetItems);
+
+        }
+
+        [Fact]
         public void Test_EditExpenseSuccess()
         {
             // Arrange
